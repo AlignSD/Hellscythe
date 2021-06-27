6f5c792b7ec975e1869ef82f01f3df118abc13e7
Evan Alioto <EvanJAlioto@gmail.com>
2021-06-27 15:18:18 -0700

    textures made for scythe not displaying in game yet

--- HellScythe/src/main/java/com/example/examplemod/ExampleMod.java
+++ HellScythe/src/main/java/com/example/examplemod/ExampleMod.java
@@ -16,7 +16,7 @@ public class ExampleMod {
         System.out.println("Hello World!");
     }
 
-    public static final String MODID = "hellscythe";
+    public static final String MODID = "sword";
     public static final String NAME = "HellScythe";
     public static final String VERSION = "1.0";
 
